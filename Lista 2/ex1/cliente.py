0020ad6beba57bd3c11adefe29a53101c824a7f0
Andressa Almeida <71462705+almeidaandressa@users.noreply.github.com>
2024-12-19 17:19:32 -0300

    Update cliente.py

--- Lista 2/ex1/cliente.py
+++ Lista 2/ex1/cliente.py
@@ -1,24 +1,20 @@
 from ftplib import FTP
 
 def run_ftp_client():
-    # Conecta ao servidor FTP
+    
     ftp = FTP()
     ftp.connect("www.inf.ufg.br")  
-    ftp.login("user", "password")   # Substituir pelo usuário e senha
+    ftp.login("user", "password")   
     
-    # Lista os arquivos no diretório remoto
     print("Arquivos no servidor FTP:")
     ftp.retrlines("LIST")
     
-    # Download de um arquivo
     with open("arquivo_baixado.txt", "wb") as f:
-        ftp.retrbinary("RETR arquivo_no_servidor.txt", f.write)  # Substituir com o nome do arquivo real no servidor
+        ftp.retrbinary("RETR arquivo_no_servidor.txt", f.write)
     
-    # Upload de um arquivo
     with open("arquivo_para_subir.txt", "rb") as f:
-        ftp.storbinary("STOR arquivo_no_servidor.txt", f)  # Substituir pelo nome do arquivo no servidor
+        ftp.storbinary("STOR arquivo_no_servidor.txt", f) 
     
-    # Desconecta do servidor
     ftp.quit()
 
 if __name__ == "__main__":
